f0aea2e9683e450910d64fccbe1e18c6a6572dc8
Ben Sigelman <bhs@lightstep.com>
2016-09-05 13:25:01 -0700

    Move README and add a package comment

---
+++ go/otgrpc/package.go
@@ -0,0 +1,4 @@
+// otgrpc provides OpenTracing support for any gRPC client or server in Go.
+//
+// See [go/otgrpc/README.md](https://github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc/README.md)
+package otgrpc
